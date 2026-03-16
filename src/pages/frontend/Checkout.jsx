b71b7a031e523fef7de90a4c1d84b4406e8a4ed4
Tetsu <ttgchang@gmail.com>
2026-03-16 16:51:02 +0800

    update: checkout

--- src/pages/frontend/Checkout.jsx
+++ src/pages/frontend/Checkout.jsx
@@ -239,67 +239,76 @@ function Checkout() {
             </tbody>
             </table>
             <h2>購物車列表</h2>
-            <div className="text-end mt-4">
-                <button
-                    type="button"
-                    className="btn btn-outline-danger"
-                    onClick={() => clearCart()}
-                >
-                    清空購物車
-                </button>
-            </div>
-            <table className="table">
-                <thead>
-                <tr>
-                    <th scope="col"></th>
-                    <th scope="col">品名</th>
-                    <th scope="col">數量/單位</th>
-                    <th scope="col">小計</th>
-                </tr>
-                </thead>
-                <tbody>
-                    {
-                        cart?.carts?.map(cartItem => (
-                        <tr key={cartItem.id}>
-                            <td>
-                            <button
-                                type="button"
-                                className="btn btn-outline-danger btn-sm"
-                                onClick={() => deleteCart(cartItem.id)}
-                            >
-                                刪除
-                            </button>
-                            </td>
-                            <th scope="row">
-                                {cartItem.product.title}
-                            </th>
-                            <td>
-                                <div className="input-group input-group-sm mb-3">
-                                    <input
-                                        type="number"
-                                        className="form-control"
-                                        aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
-                                        defaultValue={cartItem.qty}
-                                        onChange={(e) => updateCart(cartItem.id, cartItem.product_id, Number(e.target.value))}
-                                    />
-                                    <span className="input-group-text" id="inputGroup-sizing-sm">{cartItem.product.unit}</span>
-                                    </div>
-                            </td>
-                            <td className="text-end">{currency(cartItem.final_total)}</td>
-                        </tr>
+            {cart?.carts?.length === 0 && (
+                <div className="alert alert-info" role="alert">
+                    購物車是空的，快去選購吧！
+                </div>
+            )}
+            {cart?.carts?.length > 0 && (
+                <>
+                <div className="text-end mt-4">
+                    <button
+                        type="button"
+                        className="btn btn-outline-danger"
+                        onClick={() => clearCart()}
+                    >
+                        清空購物車
+                    </button>
+                </div>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">品名</th>
+                        <th scope="col">數量/單位</th>
+                        <th scope="col">小計</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart?.carts?.map(cartItem => (
+                            <tr key={cartItem.id}>
+                                <td>
+                                <button
+                                    type="button"
+                                    className="btn btn-outline-danger btn-sm"
+                                    onClick={() => deleteCart(cartItem.id)}
+                                >
+                                    刪除
+                                </button>
+                                </td>
+                                <th scope="row">
+                                    {cartItem.product.title}
+                                </th>
+                                <td>
+                                    <div className="input-group input-group-sm mb-3">
+                                        <input
+                                            type="number"
+                                            className="form-control"
+                                            aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                                            defaultValue={cartItem.qty}
+                                            onChange={(e) => updateCart(cartItem.id, cartItem.product_id, Number(e.target.value))}
+                                        />
+                                        <span className="input-group-text" id="inputGroup-sizing-sm">{cartItem.product.unit}</span>
+                                        </div>
+                                </td>
+                                <td className="text-end">{currency(cartItem.final_total)}</td>
+                            </tr>
 
-                        ))
-                    }
-                </tbody>
-                <tfoot>
-                <tr>
-                    <td className="text-end" colSpan="3">
-                    總計
-                    </td>
-                    <td className="text-end">{currency(cart.final_total)}</td>
-                </tr>
-                </tfoot>
-            </table>
+                            ))
+                        }
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <td className="text-end" colSpan="3">
+                        總計
+                        </td>
+                        <td className="text-end">{currency(cart.final_total)}</td>
+                    </tr>
+                    </tfoot>
+                </table>
+                </>
+            )}
 
             {/* 結帳頁面 */}
             <div className="my-5 row justify-content-center">
